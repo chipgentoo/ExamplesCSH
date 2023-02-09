d9374a40c7806af233fdf4a9d042168f151bf705
chipgentoo <chip.gentoo@gmail.com>
2023-02-09 23:36:15 +0300

    Example 008 Friend Dog

---
+++ Example008_Friend_Dog/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;              // просто счетчик
+double distance = 10000;    // Расстояние между друзьями
+int firstFriendSpeed = 1;   // Скорость хотьбы первого друга
+int secondFriendSpeed = 2;  // Скорость хотьбы второго друга
+int dogSpeed = 5;           // Скорость бега собаки
+int friend = 2;             // Направление бега собаки
+double time = 10;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+
+    distance = distance - (firstFriendSpeed+secondFriendSpeed)*time;
+    count++;
+}
+
+Console.WriteLine("Собака от друга к другу пробежала " + count + " раз!");
